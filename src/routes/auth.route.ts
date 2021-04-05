0d5406be087d35e5b36ffb05e3651b5e1fc83305
Lord Kay <offeilord@gmail.com>
2021-04-05 18:09:39 +0000

    Add endpoint for login and register

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,9 @@
+import * as express from "express";
+import AuthController from "../controllers/auth.controller";
+import { AuthPolicy } from "../policies/auth.policy";
+const router: express.Router = express.Router();
+export default () => {
+  router.post(`/auth/register`, [AuthController.register]);
+  router.post(`/auth/login`, [AuthPolicy.login], [AuthController.login]);
+  return router;
+};
