75642349061e0c01356431abc50f5cd17673fc55
Lord Kay <offeilord@gmail.com>
2021-04-05 12:17:13 +0000

    :sparkles: INITIAL COMMIT ❇️

---
+++ main.ts
@@ -0,0 +1,26 @@
+import * as express from "express";
+import * as helmet from "helmet";
+import * as compression from "compression";
+import * as cors from "cors";
+import { json, urlencoded } from "body-parser";
+import * as dotenv from "dotenv";
+import * as http from "http";
+dotenv.config();
+
+const server = () => {
+  const app: express.Application = express();
+  app.use(cors());
+  app.use(helmet());
+  app.use(compression());
+  app.use(json({ limit: "50mb" }));
+  app.use(urlencoded({ limit: "50mb", extended: false }));
+
+  app.set("PORT", app.get("APP_PORT") || 8090);
+
+  const server = http.createServer();
+  server.listen(app.get("PORT"), () => {
+    console.log(`Application running on port :: ${app.get("PORT")}`);
+  });
+};
+
+server();
