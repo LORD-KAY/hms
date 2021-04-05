5aba3fc31fa8ac6d5d7480d5d6d9150e4dba7e8a
Lord Kay <offeilord@gmail.com>
2021-04-05 13:33:14 +0000

    configuration for accessing env variables

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+import { env } from "./env.config";
+
+export const config = {
+  get: (key: string): any => {
+    const keys = key.split(".");
+    let variables = env;
+    for (let i = 0; i < keys.length; ++i) {
+      const key = keys[i];
+      if (key in variables) {
+        variables = variables[key];
+      }
+    }
+    return variables ?? null;
+  },
+};
