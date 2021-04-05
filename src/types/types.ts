96f560d7a601ec88a28d8dff1739edc8e99ef23b
Lord Kay <offeilord@gmail.com>
2021-04-05 18:10:27 +0000

    Add helper functions

---
+++ src/types/types.ts
@@ -0,0 +1,1 @@
+export type IUserType = "doctor" | "patient";
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,27 @@
+/**
+ * Compare the password using [isValidPassword]
+ * @param text - The raw password from the user
+ * @param password - Hashed password from the db
+ */
+import { compareSync } from "bcrypt";
+
+// @ts-ignore
+export const isValidPassword = (text: string, password: string) => {
+  return compareSync(text, password);
+};
+
+export const generateCode = (prefix: string) => {
+  return `${prefix}-${Math.floor(1000000000 + Math.random() * 9000000000)}`;
+};
+
+export const generateOrderCode = () => {
+  return Math.floor(10000 + Math.random() * 90000);
+};
+
 export const slugify = (text: string): string => {
-  return "";
+  return text
+    .replace(/\.+/g, "")
+    .replace(/\/+/g, "")
+    .replace(/\s+/g, "-")
+    .toLowerCase();
 };
